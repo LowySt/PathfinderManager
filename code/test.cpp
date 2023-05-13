3ba32fa09f16d63998df348fb97e1f970368b1cf
LoryMaster <lorenzo.stramondo@gmail.com>
2023-05-13 15:01:57 +0200

    Added file for automated tests. It's so much faster than doing it manually

---
+++ code/test.cpp
@@ -0,0 +1,58 @@
+void testAllCompendiumForAsserts()
+{
+    ls_arenaUse(compendiumArena);
+    
+    CachedPageEntry dummy = {};
+    initCachedPage(&dummy);
+    
+    b32 hasMessaged1 = FALSE;
+    b32 hasMessaged2 = FALSE;
+    b32 hasMessaged3 = FALSE;
+    b32 hasMessaged4 = FALSE;
+    b32 hasMessaged5 = FALSE;
+    
+    char buff[64] = {};
+    s32 len = 0;
+    
+    for(s32 i = 0; i < compendium.codex.pages.count; i++)
+    {
+        PageEntry pEntry = compendium.codex.pages[i];
+        CachePage(pEntry, i, &dummy, NULL);
+        
+        len = ls_utf32ToAscii_t(&dummy.name, buff, 64);
+        
+        ls_log("{string}", string({buff, len, len}));
+        
+        f32 fract = (f32)i / (f32)compendium.codex.pages.count;
+        if((fract > 0.1) && (hasMessaged1 == FALSE))  { ls_log("10% of Mobs Done"); hasMessaged1 = TRUE; }
+        if((fract > 0.25) && (hasMessaged2 == FALSE)) { ls_log("25% of Mobs Done"); hasMessaged2 = TRUE; }
+        if((fract > 0.33) && (hasMessaged3 == FALSE)) { ls_log("33% of Mobs Done"); hasMessaged3 = TRUE; }
+        if((fract > 0.50) && (hasMessaged4 == FALSE)) { ls_log("50% of Mobs Done"); hasMessaged4 = TRUE; }
+        if((fract > 0.75) && (hasMessaged5 == FALSE)) { ls_log("75% of Mobs Done"); hasMessaged5 = TRUE; }
+    }
+    
+    ls_log("Done With Mobs, beginning NPCs");
+    
+    hasMessaged1 = FALSE;
+    hasMessaged2 = FALSE;
+    hasMessaged3 = FALSE;
+    hasMessaged4 = FALSE;
+    hasMessaged5 = FALSE;
+    
+    for(s32 i = 0; i < compendium.codex.npcPages.count; i++)
+    {
+        NPCPageEntry pEntry = compendium.codex.npcPages[i];
+        CachePage(pEntry, i, &dummy, NULL);
+        
+        f32 fract = (f32)i / (f32)compendium.codex.npcPages.count;
+        if((fract > 0.1) && (hasMessaged1 == FALSE))  { ls_log("10% of NPCs Done"); hasMessaged1 = TRUE; }
+        if((fract > 0.25) && (hasMessaged2 == FALSE)) { ls_log("25% of NPCs Done"); hasMessaged2 = TRUE; }
+        if((fract > 0.33) && (hasMessaged3 == FALSE)) { ls_log("33% of NPCs Done"); hasMessaged3 = TRUE; }
+        if((fract > 0.50) && (hasMessaged4 == FALSE)) { ls_log("50% of NPCs Done"); hasMessaged4 = TRUE; }
+        if((fract > 0.75) && (hasMessaged5 == FALSE)) { ls_log("75% of NPCs Done"); hasMessaged5 = TRUE; }
+    }
+    
+    ls_log("Done With NPCs!");
+    
+    ls_arenaUse(compTempArena);
+}
